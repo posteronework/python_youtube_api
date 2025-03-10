945ed46e6e5a41aff5410f8fbf77e4997fc8019f
posteronework <posteronework@gmail.com>
2025-03-10 12:22:54 +0800

    python api final fix

--- app.py
+++ app.py
@@ -1,43 +1,3 @@
-# from flask import Flask, request, send_file, jsonify
-# import yt_dlp
-# import os
-#
-# app = Flask(__name__)
-#
-# DOWNLOAD_PATH = "downloads"
-# os.makedirs(DOWNLOAD_PATH, exist_ok=True)
-#
-#
-# @app.route("/download_audio", methods=["GET"])
-# def download_audio():
-#     youtube_url = request.args.get("url")
-#
-#     if not youtube_url:
-#         return jsonify({"error": "URL is required"}), 400
-#
-#     try:
-#         ydl_opts = {
-#             "format": "bestaudio/best",
-#             "outtmpl": f"{DOWNLOAD_PATH}/%(title)s.%(ext)s",
-#             "postprocessors": [{
-#                 "key": "FFmpegExtractAudio",
-#                 "preferredcodec": "wav",
-#                 "preferredquality": "192"
-#             }]
-#         }
-#
-#         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-#             info = ydl.extract_info(youtube_url, download=True)
-#             filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
-#
-#         return send_file(filename, as_attachment=True)
-#
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
-#
-#
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5001, debug=True)
 from flask import Flask, request, send_file, jsonify
 import yt_dlp
 import os
@@ -45,7 +5,7 @@ import os
 app = Flask(__name__)
 
 DOWNLOAD_PATH = "downloads"
-COOKIES_FILE = "cookies.txt"  # Путь к cookies
+COOKIES_FILE = "cookies.txt"
 
 os.makedirs(DOWNLOAD_PATH, exist_ok=True)
 
@@ -61,7 +21,7 @@ def download_audio():
         ydl_opts = {
             "format": "bestaudio/best",
             "outtmpl": f"{DOWNLOAD_PATH}/%(title)s.%(ext)s",
-            "cookiefile": COOKIES_FILE,  # Добавляем cookies
+            "cookiefile": COOKIES_FILE,
             "postprocessors": [{
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": "wav",
